f57e0821bc9d9436f64b5551061c464f737f0687
JLuo <zluo@gatech.edu>
2017-11-28 23:33:58 -0500

    restructuring

---
+++ messages/dialogs/GetUserInfo.js
@@ -0,0 +1,51 @@
+const builder = require('botbuilder');
+const GetClosestMatch = require('./Installation').GetClosestMatch;
+const SOFTWARE = require('./Enums').SOFTWARE;
+const OS = require('./Enums').OPERATINGSYSTEM;
+
+module.exports.GetSoftwareInfo = [
+    (session, args, next) => {
+        //list of software names
+        let choiceList = Object.keys(SOFTWARE);
+        //add in the option of other
+        choiceList.push("Other");
+        builder.Prompts.choice(session, "What software are you installing?", choiceList, {listStyle : builder.ListStyle.button});
+    },
+    (session, results, next) => {
+        if (results.response && results.response.entity) {
+            if (results.response.entity == "Other") {
+                //if user selects other, prompt them to type in the software they are looking for
+                builder.Prompts.text(session, "Please type the name of the software you are looking for");
+            } else {
+                //this should be a conversation data as it could be a different software in next interaction
+                session.conversationData.software = results.response.entity;
+                session.save();
+                session.endDialog();
+            }
+        }
+    },
+    (session, results) => {
+        if (results.response) {
+            //if user chooses to type in a name for software
+            session.conversationData.software = GetClosestMatch(Object.keys(SOFTWARE), results.response);
+            session.save();
+        }
+        session.endDialog();
+    }
+]
+
+
+module.exports.GetOSInfo = [
+    (session, args, next) => {
+        let choiceList = Object.keys(OS);
+        builder.Prompts.choice(session, "What operating system are you using?", choiceList, {listStyle : builder.ListStyle.button});
+    },
+    (session, results, next) => {
+        if (results.response && results.response.entity) {
+            console.log(results.response.entity);
+            session.userData.OS = results.response.entity;
+            session.save();
+        }
+        session.endDialog();
+    }
+]
---
+++ messages/dialogs/Installation.js
@@ -0,0 +1,102 @@
+const builder = require('botbuilder');
+const SOFTWARE = require('./Enums').SOFTWARE;
+const OS = require('./Enums').OPERATINGSYSTEM;
+
+
+const InstallationDialogs = {
+    [SOFTWARE.Matlab] : "InstallMatlab",
+    [SOFTWARE.Python] : "InstallPython",
+    [SOFTWARE.Java] : "InstallJava"
+};
+
+module.exports.ExtractUserInfo = [
+    (session, args, next) => {
+
+        //args contain the entities LUIS extracted
+        //If there is at least one entity, args would be defined
+        console.log(args);
+        if (args) {
+            //args.intent.entities is an array of entities
+            //save these instances for use later
+            for (let entityObject of args.intent.entities) {
+                switch (entityObject.type) {
+                    case 'Software':
+                        session.conversationData.software = GetClosestMatch(Object.keys(SOFTWARE), entityObject.entity);                        session.save();
+                        break;
+                    case 'OS':
+                        session.userData.OS = GetClosestMatch(Object.keys(OS), entityObject.entity);
+                        session.save();
+                        break;
+                }
+            }
+        }
+        next();
+    },
+    (session, results, next) => {
+        session.conversationData.recognizerEnabled = false;
+        session.save();
+        if (typeof session.conversationData.software === 'undefined') {
+            session.beginDialog('GetSoftwareInfo');
+        }
+    },
+    (session, results, next) => {
+        session.conversationData.recognizerEnabled = true;
+        session.save();
+        session.send(`Let's help you install ${session.conversationData.software}`);
+        //depends on what software the user wants to Install
+        //start corresponding tutorial
+        session.beginDialog(InstallationDialogs[session.conversationData.software]);
+    }
+]
+
+
+const Levenshtein_Distance = function (str1, str2) {
+    if (str1.length == 0) {
+        return str2.length;
+    } else if (str2.length == 0) {
+        return str1.length;
+    }
+
+    let matrix = [];
+
+    //put str1 on the left
+    for (let i = 0; i <= str1.length; i++) {
+        matrix[i] = [i];
+    }
+
+    //put str2 on the top
+    for (let i = 0; i <= str2.length; i++) {
+        matrix[0][i] = i;
+    }
+
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            let substitutionCost = str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1;
+            matrix[i][j] = Math.min(
+                Math.min(
+                    matrix[i - 1][j] + 1,
+                    matrix[i][j - 1] + 1
+                ),
+                matrix[i - 1][j - 1] + substitutionCost
+            );
+        }
+    }
+
+    return matrix[str1.length][str2.length];
+}
+
+const GetClosestMatch = function (choiceList, userInput) {
+    let closetMatch = choiceList[0];
+    let minimalEditDistance = userInput.length;
+    for (let choice of choiceList) {
+        let currentChoiceDistance = Levenshtein_Distance(choice, userInput);
+        if (currentChoiceDistance < minimalEditDistance) {
+            closetMatch = choice;
+            minimalEditDistance = currentChoiceDistance;
+        }
+    }
+    return closetMatch;
+}
+
+module.exports.Levenshtein_Distance = Levenshtein_Distance;
+module.exports.GetClosestMatch = GetClosestMatch;
