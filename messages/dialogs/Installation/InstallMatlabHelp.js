3520b6b1d954907475612f07c5da5b4f4fd3f637
JLuo <jeffreyzcluo@gmail.com>
2017-12-02 14:34:34 -0500

    added help intent and dialog for help during installation steps

--- messages/dialogs/Installation/InstallMatlabHelp.js
+++ messages/dialogs/Installation/InstallMatlabHelp.js
@@ -2,6 +2,7 @@
 module.exports.InstallMatlabHelp_Windows = [
     (session, args, next) => {
         session.send("let's help you with installation of matlab");
+        next();
     },
     (session, results, next) => {
         session.endDialog("Returning to previous step");
