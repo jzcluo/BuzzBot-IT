d1d416cf80ddcb2ccd70cf917454bc2106e82607
JLuo <jeffreyzcluo@gmail.com>
2017-12-06 10:35:59 -0500

    added help message

--- messages/index.js
+++ messages/index.js
@@ -2,9 +2,9 @@
 const builder = require("botbuilder");
 const botbuilder_azure = require("botbuilder-azure");
 const path = require('path');
-//if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV == "development") {
     require('dotenv').config();
-//}
+}
 
 
 //These dialogs are difined within the dialogs folder
